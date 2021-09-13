24a330505aeb17581bfdb9f79758520c759577d1
frans203 <franciscosantana203@gmail.com>
2021-09-13 14:14:26 -0300

    First commit

---
+++ precache-manifest.184d3c6925e7f6f2239ae42bf33fe5c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27c410141618a6b5ee644f0932aba3dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4f321a0571df729c4f1",
+    "url": "/static/css/main.c69bebdf.chunk.css"
+  },
+  {
+    "revision": "f509fada7c0f695c7e3f",
+    "url": "/static/js/2.01644629.chunk.js"
+  },
+  {
+    "revision": "f4f321a0571df729c4f1",
+    "url": "/static/js/main.beb4b508.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
